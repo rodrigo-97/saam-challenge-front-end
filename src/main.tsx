85bb91e4fb36c06e97ce0548f90dd3e34b940d87
Rodrigo Araújo <rodrigo.dev99@gmail.com>
2025-12-10 13:59:53 -0300

    feat: setup tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./app.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById("root");
+if (!container) {
+	throw new Error('Root element with id "root" not found');
+}
+
+createRoot(container).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "node:path";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+			generatedRouteTree: path.resolve("src/routes.gen.ts"),
+			quoteStyle: "double",
+			routeFileIgnorePrefix: "@",
+		}),
+		react(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve("src"),
+		},
+	},
+});
